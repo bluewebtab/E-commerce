869daa81cce73ada13cb9e3fa5c1453238e9df70
Ronny Gutierrez <ronny.icne@gmail.com>
2018-04-14 21:54:40 -0700

    update 9:54pm

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/E-commerce');
 var Product = require('./model/product');
 
-var wishList = require('./model/wishlist')
+var WishList = require('./model/wishlist')
 
 
 //Parse means to interpret data
 app.use(bodyParser.json());//tells the system you want json to be used.
 app.use(bodyParser.urlencoded({extended:false}));// tells the system you want a simple algorithm for  shallow parsing by using false
+
+
+
+
+
+
+
 
 
 
@@ -28,6 +35,69 @@ app.post('/product', function(request, response){
     });
    
 });
+
+app.get('/product', function(request, response){
+   Product.find({}, function(err, products){
+    if(err){
+        response.status(500).send({error: "Could not fetch products"});
+    }else{
+        response.send(products);
+    }   
+   });
+    
+});
+
+
+
+
+
+
+
+
+
+
+app.get('/wishlist', function(request, response){
+    WishList.find({}).populate({path: 'products', model: 'Product'}).exec(function(err,wishLists){
+        if(err){
+            response.status(500).send({error: "Could not fetch wishLists"});
+        }else{
+            response.status(200).send(wishLists);
+        }
+    });
+});
+
+app.post('/wishlist', function(request,response){
+    var wishList = new WishList();
+    wishList.title = request.body.title;
+    wishList.save(function(err, newWishList){
+        if(err){
+            response.status(500).send({error: "Could not create a wishlist."});
+        }else{
+            response.send(newWishList);
+        }
+    });
+});
+
+
+app.put('/wishlist/product/add', function(request, response){
+   Product.findOne({_id: request.body.productId}, function(err, product){
+       if(err){
+           response.status(500).send({error: "Could not add in product to wishlist"});
+       }else{
+           WishList.update({_id: request.body.wishListId}, {$addToSet:{products: product._id}}, function(err, wishList){
+              if(err) {
+                 response.status(500).send({error: "Could not add in product to wishlist"}); 
+              }else{
+                  response.send(wishList);
+              }
+           });
+       }
+   });
+});
+
+
+
+
 
 app.listen(3000, function(){
     console.log("E-commerce api running on port 3000...");
